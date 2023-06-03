f0e380e4566274f2d72b7060bacafe8268bb7a3f
Phi Huy Hoang <luumanhlapdi@gmail.com>
2023-06-03 15:27:27 +0200

    deploy

--- Hubsson.Hackathon.Arcade.Client.Dotnet/Hubsson.Hackathon.Arcade.Client.Dotnet/Services/MatchService.cs
+++ Hubsson.Hackathon.Arcade.Client.Dotnet/Hubsson.Hackathon.Arcade.Client.Dotnet/Services/MatchService.cs
@@ -16,8 +16,16 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
         int MapHeight;
         double tickMs;
 
+        int IndexOfOurTeam;
+
+        int OurlengthCoordinate;
+        int ourCurrentX ;
+        int ourCurrentY;
+
         Domain.Action action;
 
+        int initial = 0;
+        bool[,] grid;
         public MatchService(ArcadeSettings settings, ILogger<MatchService> logger)
         {
             _logger = logger;
@@ -28,6 +36,7 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
         public void Init()
         {
             _logger.LogInformation("Init is running");
+            initial += 1;     
 
         }
 
@@ -40,23 +49,51 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
         {
             return allDistance.MinBy(kvp => kvp.Value).Key;
         }
+
+        private static List<string> RemoveWhereOccupied(int x, int y, bool[,] grid, int MapWidth, int MapHeight, List<string> validDirections)
+        {
+            if (x > 0 && grid[x - 1, y] && validDirections.Any(d => d == "Right")) { }
+            validDirections.Remove("Right"); // can't go left
+            if (x < MapWidth -1 && grid[x + 1, y] && validDirections.Any(d => d == "Left"))
+                validDirections.Remove("Left"); // can't go right
+            if (y > 0 && grid[x, y - 1] && validDirections.Any(d => d == "Up"))
+                validDirections.Remove("Up"); // can't go up
+            if (y < MapHeight -1 && grid[x, y + 1] && validDirections.Any(d => d == "Down"))
+                validDirections.Remove("Down"); // can't go down
+
+           // _logger.LogInformation("here: {0} {1} {2}", x.ToString(), y.ToString(), string.Join(",",validDirections));
+            return validDirections;
+        }
+        
+
+        private static string RandomMouse(List<string> directions)
+        {
+            var randomiser = new Random(DateTime.Now.Millisecond);
+            return directions[randomiser.Next(directions.Count)];
+        }
         public Hubsson.Hackathon.Arcade.Client.Dotnet.Domain.Action Update(ClientGameState gameState)
         {
+            List<string> directions = new List<string> { "Up", "Down", "Left", "Right" };
+            if (initial <= 1)
+            {
+                tickMs = gameState.tickTimeInMs;
+                MapWidth = gameState.width;
+                MapHeight = gameState.height;
+                grid = new bool[MapWidth, MapHeight];
+                IndexOfOurTeam = Array.FindIndex(gameState.players, player => player.playerId == _arcadeSettings.TeamId);
 
-            tickMs = gameState.tickTimeInMs;
-            MapWidth = gameState.width;
-            MapHeight = gameState.height;
+            }
+            initial = 2;
+            _logger.LogInformation(initial.ToString());
 
             Dictionary<string, double> allDistance = new Dictionary<string, double>();
-
             int numberOfPlayers = gameState.players.Length;
-
-            int IndexOfOurTeam = Array.FindIndex(gameState.players, player => player.playerId == _arcadeSettings.TeamId);
-
-            int OurlengthCoordinate = gameState.players[IndexOfOurTeam].coordinates.Length;
-            int ourCurrentX = gameState.players[IndexOfOurTeam].coordinates[OurlengthCoordinate - 1].x;
-            int ourCurrentY = gameState.players[IndexOfOurTeam].coordinates[OurlengthCoordinate - 1].y;
-
+            OurlengthCoordinate = gameState.players[IndexOfOurTeam].coordinates.Length;
+            ourCurrentX = gameState.players[IndexOfOurTeam].coordinates[OurlengthCoordinate - 1].x;
+            ourCurrentY = gameState.players[IndexOfOurTeam].coordinates[OurlengthCoordinate - 1].y;
+            _logger.LogInformation("{0}, {1}",ourCurrentX.ToString(), ourCurrentY.ToString());
+            //grid[ourCurrentX, ourCurrentY] = true;
+            
             for (int i = 0; i < numberOfPlayers; i++)
             {
                 if (i != IndexOfOurTeam)
@@ -67,56 +104,67 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
                     {
                         int currentX = gameState.players[i].coordinates[lengthCoordinate - 1].x;
                         int currentY = gameState.players[i].coordinates[lengthCoordinate - 1].y;
+                        grid[currentX,currentY] = true;
                         double d = DistanceBetweenPlayer(ourCurrentX, ourCurrentY, currentX, currentY);
                         allDistance[playerID] = d;
-                        _logger.LogInformation("distance between {0} and us is {1}", playerID, d.ToString());
+                        //_logger.LogInformation("distance between {0} and us is {1}", playerID, d.ToString());
                     }
 
                 }
 
             }
 
-            _logger.LogInformation("best player is: {0}", MinDistance(allDistance));
+            var validDirections = RemoveWhereOccupied(ourCurrentX,ourCurrentY, grid, MapWidth, MapHeight, directions);
+            _logger.LogInformation("valid {0} grid {1}", string.Join(",",validDirections), grid[ourCurrentX-1,ourCurrentY-1].ToString());
+            var nextMove = RandomMouse(validDirections);
 
-            int IndexOfBestPlayer = Array.FindIndex(gameState.players, player => player.playerId == MinDistance(allDistance));
-            int BestPlayerlengthCoordinate = gameState.players[IndexOfBestPlayer].coordinates.Length;
-            int bestPlayerX = gameState.players[IndexOfBestPlayer].coordinates[BestPlayerlengthCoordinate - 1].x;
-            int bestPlayerY = gameState.players[IndexOfBestPlayer].coordinates[BestPlayerlengthCoordinate - 1].y;
+            Domain.Direction myEnum = (Domain.Direction)Enum.Parse(typeof(Domain.Direction), nextMove);
 
-            int x0 = ourCurrentX - bestPlayerX;
-            int y0 = ourCurrentY - bestPlayerY;
-            if (x0 < y0 && y0 >= 0 && ourCurrentY < MapHeight)
+            action = new Domain.Action()
             {
-                action = new Domain.Action()
-                {
-                    direction = Domain.Direction.Up,
-                    iteration = gameState.iteration
-                };
-            }
-            else if (x0 < y0 && y0 < 0 && ourCurrentX < MapWidth)
-            {
-                action = new Domain.Action()
-                {
-                    direction = Domain.Direction.Down,
-                    iteration = gameState.iteration
-                };
-            }
-            else if (x0 > y0 && x0 >= 0)
-            {
-                action = new Domain.Action()
-                {
-                    direction = Domain.Direction.Left,
-                    iteration = gameState.iteration
-                };
-            }
-            else
-            {
-                action = new Domain.Action()
-                {
-                    direction = Domain.Direction.Right,
-                    iteration = gameState.iteration
-                };
-            }
+                direction = myEnum,
+                iteration = gameState.iteration
+            };
+
+            //int IndexOfBestPlayer = Array.FindIndex(gameState.players, player => player.playerId == MinDistance(allDistance));
+            //int BestPlayerlengthCoordinate = gameState.players[IndexOfBestPlayer].coordinates.Length;
+            //int bestPlayerX = gameState.players[IndexOfBestPlayer].coordinates[BestPlayerlengthCoordinate - 1].x;
+            //int bestPlayerY = gameState.players[IndexOfBestPlayer].coordinates[BestPlayerlengthCoordinate - 1].y;
+
+            //int x0 = ourCurrentX - bestPlayerX;
+            //int y0 = ourCurrentY - bestPlayerY;
+            //if (x0 < y0 && y0 >= 0 && ourCurrentY < MapHeight)
+            //{
+            //    action = new Domain.Action()
+            //    {
+            //        direction = Domain.Direction.Up,
+            //        iteration = gameState.iteration
+            //    };
+            //}
+            //else if (x0 < y0 && y0 < 0 && ourCurrentX < MapWidth)
+            //{
+            //    action = new Domain.Action()
+            //    {
+            //        direction = Domain.Direction.Down,
+            //        iteration = gameState.iteration
+            //    };
+            //}
+            //else if (x0 > y0 && x0 >= 0)
+            //{
+            //    action = new Domain.Action()
+            //    {
+            //        direction = Domain.Direction.Left,
+            //        iteration = gameState.iteration
+            //    };
+            //}
+            //else
+            //{
+            //    action = new Domain.Action()
+            //    {
+            //        direction = Domain.Direction.Right,
+            //        iteration = gameState.iteration
+            //    };
+            //}
 
 
             return action;
@@ -125,7 +173,6 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
 
         public class MatchRepository
         {
-            public ClientGameState[] _clientGameState { get; set; }
         }
     }
 }
